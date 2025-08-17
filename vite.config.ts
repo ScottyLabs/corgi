b1b4cd4f6fe4831f5a4e0bad4ed534fd749c8074
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-08-17 02:04:56 -0400

    chore: add back __dirname

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { resolve } from "node:path";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-oxc";
 import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
