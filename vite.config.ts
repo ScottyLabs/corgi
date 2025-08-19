75c6946c1c89a787cb5b7f227e5796a96968994e
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-08-18 22:54:44 -0400

    fix: add back the vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,46 @@
-import { dirname, resolve } from "node:path";
+import { dirname, extname, relative, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-oxc";
+import { glob } from "glob";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        dts({
+            include: ["lib"],
+            tsconfigPath: resolve(__dirname, "tsconfig.app.json"),
+        }),
+    ],
     build: {
-        copyPublicDir: false, // Don't include public/ in dist/
         lib: {
             entry: resolve(__dirname, "lib/main.ts"),
             formats: ["es"],
+        },
+        rollupOptions: {
+            external: ["react", "react/jsx-runtime"],
+            input: Object.fromEntries(
+                glob.sync("lib/**/*.{ts,tsx}").map((file) => [
+                    // The name of the entry point
+                    // lib/nested/foo.ts becomes nested/foo
+                    relative(
+                        "lib",
+                        file.slice(0, file.length - extname(file).length),
+                    ),
+                    // The absolute path to the entry file
+                    // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+                    fileURLToPath(new URL(file, import.meta.url)),
+                ]),
+            ),
+            output: {
+                entryFileNames: "[name].js",
+            },
         },
     },
 });
